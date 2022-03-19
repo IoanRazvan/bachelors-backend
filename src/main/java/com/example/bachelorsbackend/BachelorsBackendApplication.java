bd120855b0d2aed761516c0aa14aee4c9fd8d3a4
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-03-19 09:22:11 +0200

    initial commit

---
+++ src/main/java/com/example/bachelorsbackend/BachelorsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bachelorsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BachelorsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bachelorsbackend/BachelorsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bachelorsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

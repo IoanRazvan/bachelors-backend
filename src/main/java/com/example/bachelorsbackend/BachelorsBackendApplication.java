c719ca5d0ac79a9b5f14e3746eaf1e36590460da
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-06-03 01:49:19 +0300

    Added object array to problem row dto converter

--- src/main/java/com/example/bachelorsbackend/BachelorsBackendApplication.java
+++ src/main/java/com/example/bachelorsbackend/BachelorsBackendApplication.java
@@ -1,7 +1,9 @@
 package com.example.bachelorsbackend;
 
+import com.example.bachelorsbackend.converters.ObjectArrayToProblemRowDTOConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class BachelorsBackendApplication {
         SpringApplication.run(BachelorsBackendApplication.class, args);
     }
 
+    @Bean
+    ObjectArrayToProblemRowDTOConverter converter() {
+        return new ObjectArrayToProblemRowDTOConverter();
+    }
 }

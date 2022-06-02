6c40a0a0d707c9d09e535c9b528dc021712915bc
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-06-03 01:40:15 +0300

    Modified PageFactory implementation

--- src/main/java/com/example/bachelorsbackend/dtos/PageFactory.java
+++ src/main/java/com/example/bachelorsbackend/dtos/PageFactory.java
@@ -2,6 +2,7 @@ package com.example.bachelorsbackend.dtos;
 
 import org.springframework.data.domain.Slice;
 
+import java.util.Map;
 import java.util.function.Function;
 
 public class PageFactory {
@@ -9,16 +10,15 @@ public class PageFactory {
         return new Page<>(slice);
     }
 
+    public static <U> Page<U> of(Slice<U> slice, Map<String, Object> parameters) {
+        return new ParameterizedPage<>(slice, parameters);
+    }
+
     public static <T1, T2> Page<T2> of(Slice<T1> slice, Function<? super T1, ? extends T2> mapper) {
         return new Page<>(slice.map(mapper));
     }
 
-    public static <T1, T2> Page<T2> of(Slice<T1> slice, Function<? super T1, ? extends T2> mapper, String query, String sorting) {
-        return of(slice, mapper, query, sorting, "");
+    public static <T1, T2> Page<T2> of(Slice<T1> slice, Function<? super T1, ? extends T2> mapper, Map<String, Object> parameters) {
+        return new ParameterizedPage<>(slice.map(mapper), parameters);
     }
-
-    public static <T1, T2> Page<T2> of(Slice<T1> slice, Function<? super T1, ? extends T2> mapper, String query, String sorting, String additional) {
-        return new SortedQueryPage<>(slice.map(mapper), query, sorting, additional);
-    }
-
 }

393b25a738710d8278ef27b17a77b4e1e4d2203d
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-06-03 02:41:04 +0300

    Added problem difficulty to response dto

--- src/main/java/com/example/bachelorsbackend/dtos/ProblemResponseDTO.java
+++ src/main/java/com/example/bachelorsbackend/dtos/ProblemResponseDTO.java
@@ -1,6 +1,7 @@
 package com.example.bachelorsbackend.dtos;
 
 import com.example.bachelorsbackend.models.Category;
+import com.example.bachelorsbackend.models.ProblemDifficulty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ public class ProblemResponseDTO {
     private int id;
     private String title;
     private String description;
+    private ProblemDifficulty difficulty;
     private List<ProblemStarterDTO> starters;
     private List<Category> categories;
 }
